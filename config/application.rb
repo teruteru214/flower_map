92cad6dc4d7509697e799d285c0e965b2f7d3422
teruteru <tutituti3080@gmail.com>
2022-11-28 11:44:03 +0900

    Update: ジェネレートコマンドでルートとヘルパーとテストを生成しないように設定

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,11 @@ module FlowerMap
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.skip_routes true
+      g.helper false
+      g.test_framework false
+    end
   end
 end
