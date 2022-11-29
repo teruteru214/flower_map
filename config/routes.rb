7e150ce9a56f450207258ef6476993672268c375
teruteru <tutituti3080@gmail.com>
2022-11-29 20:48:46 +0900

    Fix: ルーティングの記述ミスの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :flowers do
-        get 'today_flower' to: 'today_flower#index'
+        get 'today_flower', to: 'today_flower#index'
       end
       post 'login', to: 'user_sessions#create'
       delete 'logout', to: 'user_sessions#destroy'
